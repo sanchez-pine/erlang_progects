94638b3107215f93a211e2ea9469eb939e5ec36b
Alex Sosna <alex@mail.ru>
2024-07-11 14:25:22 +0000

    This is my first project on Erlang

---
+++ funbox/src/funbox_number.erl
@@ -0,0 +1,29 @@
+-module(funbox_number).
+-export([is_prime/1, random/2]).
+
+is_prime(N) ->
+    is_prime(N, 2, erlang:trunc(math:sqrt(N)+1)).
+
+is_prime(_, Max, Max) ->
+    true;
+is_prime(N, I, Max)   ->
+    if
+	N rem I =:= 0, is_integer(N) ->
+	    false;
+	true ->
+	    is_prime(N, I+1, Max)
+    end.
+
+random(N, Count) when N > 2  -> 
+    random(N, Count, 0, []).
+
+random(_, Max, Max, Acc) -> 
+    Acc;
+random(N, Count, I, Acc)     ->
+    H = rand:uniform(N),
+    if
+        H =:= 1 ->
+            random(N, Count, I+1, [H+1|Acc]);
+        H > 1   ->
+            random(N, Count, I+1, [H|Acc])
+    end.
